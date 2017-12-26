9215e0c6e99d53b168ef252df9ab00241bbc81f8
wanglinben <com020888@126.com>
2017-12-27 00:19:17 +0800

    aa

---
+++ public/src/main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Greeter from './js/index';
+
+ReactDom.render(<Greeter/>, document.getElementById('root'));
+
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+
+module.exports = {
+        entry: __dirname + "/public/src/main.js", //已多次提及的唯一入口文件
+        output: {
+            path: __dirname + "/public/build",
+            filename: "[name].js"
+        },
+        devtool: 'eval-source-map',
+        module: {
+            rules: [{
+                    test: /(\.jsx|\.js)$/,
+                    use: {
+                        loader: "babel-loader"
+                    },
+                    exclude: /node_modules/
+                },
+                {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, {
+                        loader: "css-loader"
+                    }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+        ]
+};
